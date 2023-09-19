8582ee65dd2e440158894fdb79712e4c4ea4a0b9
mariafernandabarbie <maria.souza27@estudante.ifms.edu.br>
2023-09-19 16:45:16 -0400

    alter

--- src/app/pages/dashboard/alter/page.js
+++ src/app/pages/dashboard/alter/page.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function Register() {
     const handler = async (e) => {
         e.preventDefault();
-        toast('Registrado com sucesso')
+        toast('Alterado com sucesso')
       }
     
     return (
